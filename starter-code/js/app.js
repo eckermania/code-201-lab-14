b304dd59df31a9b404100b5a808cab6f211abb81
eckermania <erineckerman@gmail.com>
2018-08-23 14:41:04 -0700

    merging work

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -8,15 +8,23 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  new CartItem(product, quantity);
+  this.items.push(CartItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cart-contents', JSON.stringify(Cart.items));
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  var item = CartItem.product;
+  //TODO: should the above be just CartItem instead?
+  var cartIndex = Cart.items.indexOf(item);
+  Cart.items.splice(cartIndex, 1);
+  localStorage.removeItem('cart-contents', item);
 };
 
 var CartItem = function(product, quantity) {
